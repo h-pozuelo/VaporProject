2b43bdc30b6b190c72b0653cbc144bee814f67b5
Cristian Bolarin <Cristian.bolarin@gmail.com>
2024-04-05 20:59:57 +0200

    pendiente html biblioteca

--- Front-end/client/src/app/pages/biblioteca/biblioteca.component.ts
+++ Front-end/client/src/app/pages/biblioteca/biblioteca.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BibliotecaService } from '../../core/services/biblioteca.service';
 import { AuthenticationService } from '../../core/services/authentication.service';
+import { Observable, take, tap } from 'rxjs';
+import { IJuegoResults } from '../../interfaces/juego';
+import { JuegosService } from '../../core/services/juegos.service';
+import { IBiblioteca } from '../../interfaces/Biblioteca';
 
 @Component({
   selector: 'app-biblioteca',
@@ -10,33 +14,45 @@ import { AuthenticationService } from '../../core/services/authentication.servic
   styleUrl: './biblioteca.component.css'
 })
 export class BibliotecaComponent implements OnInit {
+  public juegoResults$!: Observable<IJuegoResults>;
 
   constructor(
     private bibliotecaService: BibliotecaService,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private juegosService: JuegosService
   ) { }
 
   //suscribir  authservice para obtener el id del usuario
   //llamar al servicio de biblioteca para obtener la biblioteca del usuario
   //guardar la biblioteca en una variable (id)  despues pasamos el id al getBiblioteca
 
-
+  
   ngOnInit(): void {
     this.authService.authChanged.subscribe((isAuthenticated) => {
       if (isAuthenticated) {
         this.authService.getUserEmail();
       }
     });
+    
+    // this.juegoResults$ = this.bibliotecaService.getBiblioteca()
+    // .pipe(take(1))
+
   }
-
-
-  getBiblioteca(id: string) {
-    this.bibliotecaService.getBiblioteca(id)
-      .subscribe((data) => {
+  
+  getBiblioteca(id: string): Observable<IBiblioteca[]> {
+    return this.bibliotecaService.getBiblioteca(id)
+      .pipe(tap((data) => {
         console.log(data);
-      });
+      }));
   }
-
+  
+  // getBiblioteca(id: string) {
+  //   this.bibliotecaService.getBiblioteca(id)
+  //     .subscribe((data) => {
+  //       console.log(data);
+  //     });
+  // }
+  
 
 
 
