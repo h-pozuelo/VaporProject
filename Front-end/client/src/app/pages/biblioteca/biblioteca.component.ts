b81081ad8dbd56e8ac89d0d9c836892905c611be
Cristian Bolarin <cristian.bolarin@gmail.com>
2024-04-05 14:42:45 +0200

    m

--- Front-end/client/src/app/pages/biblioteca/biblioteca.component.ts
+++ Front-end/client/src/app/pages/biblioteca/biblioteca.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BibliotecaService } from '../../core/services/biblioteca.service';
+import { AuthenticationService } from '../../core/services/authentication.service';
 
 @Component({
   selector: 'app-biblioteca',
@@ -8,13 +9,37 @@ import { BibliotecaService } from '../../core/services/biblioteca.service';
   templateUrl: './biblioteca.component.html',
   styleUrl: './biblioteca.component.css'
 })
-export class BibliotecaComponent implements OnInit{
+export class BibliotecaComponent implements OnInit {
 
-constructor(private bibliotecaService:BibliotecaService) {}
+  constructor(
+    private bibliotecaService: BibliotecaService,
+    private authService: AuthenticationService
+  ) { }
 
-//suscribir  authservice para obtener el id del usuario
-//llamar al servicio de biblioteca para obtener la biblioteca del usuario
-//guardar la biblioteca en una variable (id)  despues pasamos el id al getBiblioteca
+  //suscribir  authservice para obtener el id del usuario
+  //llamar al servicio de biblioteca para obtener la biblioteca del usuario
+  //guardar la biblioteca en una variable (id)  despues pasamos el id al getBiblioteca
+
+
+  ngOnInit(): void {
+    this.authService.authChanged.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.authService.getUserEmail();
+      }
+    });
+  }
+
+
+  getBiblioteca(id: string) {
+    this.bibliotecaService.getBiblioteca(id)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+
+
+
 
 
 }
