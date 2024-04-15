9c1e659009f43fd4b05edbfc220b49fd62c95d86
CristianB891 <cristian.bolarin@gmail.com>
2024-04-15 11:08:59 +0200

    subida a main del home

--- Front-end/client/src/app/pages/home/home.component.ts
+++ Front-end/client/src/app/pages/home/home.component.ts
@@ -10,4 +10,34 @@ import { LoginComponent } from '../login/login.component';
 })
 export class HomeComponent {
 
+  ImagenesUrl: string[] = [
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/292030/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/271590/capsule_616x353.jpg',
+    'https://cdn.akamai.steamstatic.com/steam/apps/1091500/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/730/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/1172470/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/1240930/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/343594/capsule_616x353.jpg',
+    'https://cdn.akamai.steamstatic.com/steam/apps/2215430/header.jpg?t=1712313893',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/945360/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/570/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/252950/capsule_616x353.jpg',
+    
+  ].concat([
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/292030/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/271590/capsule_616x353.jpg',
+    'https://cdn.akamai.steamstatic.com/steam/apps/1091500/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/730/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/1172470/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/1240930/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/343594/capsule_616x353.jpg',
+    'https://cdn.akamai.steamstatic.com/steam/apps/2215430/header.jpg?t=1712313893',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/945360/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/570/capsule_616x353.jpg',
+    'https://cdn.cloudflare.steamstatic.com/steam/apps/252950/capsule_616x353.jpg',
+  ]);
+  
+
 }
